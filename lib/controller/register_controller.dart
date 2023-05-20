141fc31a9fc92ca8a3ff2c37e796c6add0a20f63
IKhlash02 <muhammadikhlash5@gmail.com>
2023-05-20 07:23:21 +0700

    register

--- lib/controller/register_controller.dart
+++ lib/controller/register_controller.dart
@@ -3,9 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'package:get_healt/screen/navbar_button.dart';
 import 'package:get_healt/util/api_endpoint.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../screen/succes_register.dart';
 
--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:get_healt/controller/register_controller.dart';
-import 'package:get_healt/screen/succes_register.dart';
+
 import 'package:get_healt/widget/dropdown.dart';
 import 'package:get_healt/widget/submit_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../util/colors.dart';
 import '../controller/id_provinsi.dart';
-import '../widget/button_input.dart';
+
 import '../widget/button_login.dart';
-import '../widget/button_submit.dart';
+
 import '../widget/dropdown_districk.dart';
 import '../widget/dropdown_regency.dart';
 import '../widget/dropdown_village.dart';
